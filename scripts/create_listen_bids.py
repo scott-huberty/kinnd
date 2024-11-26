f0dd087a5524602a7e696bf986a25f81b9c2764b
Scott Huberty <seh33@uw.edu>
2024-11-26 14:45:13 -0800

    Update scripts

--- scripts/create_listen_bids.py
+++ scripts/create_listen_bids.py
@@ -43,13 +43,14 @@ EVENT_IDS = {
             "IEND": 65,
             }
 
-def bidsify_listen_files(task=None, overwrite=False):
+def bidsify_listen_files(subject=None, session=None, task=None, overwrite=False):
     """BIDS Sanitize the Listen EEG files."""
-    broot = kinnd.utils.paths.listen_path().parent / "data" / "bids"
+    broot = kinnd.utils.paths.get_listen_path() / "data" / "bids"
     fname = broot.parent / "eeg_list.csv"
     df = pd.read_csv(fname, header=0)
-
     for tup in df.itertuples():
+        if subject is not None and str(tup.subject) != str(subject):
+             continue
         if pd.isnull(tup.task):
             warn(
                  f"Skipping {tup.subject}_ses-{tup.session:02d} because it has no task.",
@@ -67,49 +68,75 @@ def bidsify_listen_files(task=None, overwrite=False):
                          stacklevel=2,
                          )
                     continue
+        bidsify_one_file(tup, bids_root=broot, overwrite=overwrite)
 
-        if task == "semantics":
-            event_mapping = {"img+": "image", "snd+": "word"}
-        elif task == "phonemes":
-            event_mapping = {"stm+": "tone"}
-        else:
-            event_mapping = None
-        if tup.task == "phonemes" and tup.subject in [2055, 2058, 2068]:
-            # Special case where this is missing from the file
-            condition_mapping = {1: "Standard", 2: "Deviant"}
-        else:
-            condition_mapping = None
-        raw = kinnd.studies.listen.io.read_raw_listen(
-            tup.sourcefile,
-            event_mapping=event_mapping,
-            condition_mapping=condition_mapping,
-            )
 
-        subject = str(tup.subject)
-        session = f"{tup.session:02d}"
+def bidsify_one_file(pd_tuple, *, bids_root, overwrite=False):
+    subject = pd_tuple.subject
+    session = pd_tuple.session
+    task = pd_tuple.task
+    sourcefile = pd_tuple.sourcefile
 
-        bpath = mne_bids.BIDSPath(
-            subject=subject,
-            session=session,
-            task=tup.task,
-            suffix="eeg",
-            datatype="eeg",
-            root=broot,
-            )
+    if task == "semantics":
+        event_mapping = {"img+": "image", "snd+": "word"}
+    elif task == "phonemes":
+        event_mapping = {"stm+": "tone"}
+    else:
+        event_mapping = None
+    if task == "phonemes" and subject in [2055, 2058, 2068]:
+        # Special case where this is missing from the file
+        condition_mapping = {1: "Standard", 2: "Deviant"}
+    else:
+        condition_mapping = None
+    raw = kinnd.studies.listen.io.read_raw_listen(
+        sourcefile,
+        event_mapping=event_mapping,
+        condition_mapping=condition_mapping,
+        )
 
-        mne_bids.write.write_raw_bids(
-            raw=raw,
-            bids_path=bpath,
-            overwrite=overwrite,
-            event_id=EVENT_IDS,
-            allow_preload=True,
-            format="EDF",
-            )
+    subject = str(subject)
+    session = f"{session:02d}"
 
+    bpath = mne_bids.BIDSPath(
+        subject=subject,
+        session=session,
+        task=task,
+        suffix="eeg",
+        datatype="eeg",
+        root=bids_root,
+        )
+
+    mne_bids.write.write_raw_bids(
+        raw=raw,
+        bids_path=bpath,
+        overwrite=overwrite,
+        event_id=EVENT_IDS,
+        allow_preload=True,
+        format="EDF",
+        )
+    return True
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--task", dest="task", default=None, type=str)
-    parser.add_argument("--overwrite", dest="overwrite", default=False, choices=[True, False], type=bool)
+    parser.add_argument("--subject", dest="subject", default=None, type=str)
+    parser.add_argument("--session", dest="session", default=None, type=str)
+    parser.add_argument(
+         "--task",
+         dest="task",
+         default=None,
+         type=str,
+         choices=["phonemes", "semantics", "resting", None]
+         )
+    parser.add_argument(
+         "--overwrite",
+         dest="overwrite",
+         default=False,
+         choices=[True, False],
+         type=bool
+         )
     args = parser.parse_args()
-    bidsify_listen_files(task=args.task, overwrite=args.overwrite)
+    bidsify_listen_files(
+         subject=args.subject,
+         session=args.session,task=args.task,
+         overwrite=args.overwrite
+         )
--- scripts/process_listen.py
+++ scripts/process_listen.py
@@ -1,5 +1,9 @@
 import argparse
 
+import os
+import time
+
+from datetime import datetime
 from pathlib import Path
 from warnings import warn
 
@@ -12,27 +16,67 @@ import pylossless as ll
 
 import kinnd
 
+import logging
 
-def process_one_subject(bpath):
+
+def process_one_subject(bpath, overwrite):
     """Process a single subject from the Listen study."""
-    config_fpath = get_pylossless_config(bpath)
-
     subject = bpath.subject
     session = bpath.session
     task = bpath.task
     broot = bpath.root
-    raw = mne_bids.read_raw_bids(bpath)
-    raw.info["bads"].extend(["E125", "E126", "E127", "E128"])
 
-    # Save Outputs
+    # instantiate logger
+    logger = mne.utils.logger
+    # logger.setLevel("DEBUG")
+
+    console_handler = logging.StreamHandler()
+    log_fpath = str(Path(__file__).parent / f"./logs/sub-{subject}_ses-{session}_task-{task}.txt")
+    file_handler = logging.FileHandler(log_fpath, mode="a", encoding="utf-8")
+    formatter = logging.Formatter(
+        "{asctime} - {levelname} - {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M",
+    )
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel("DEBUG")
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel("DEBUG")
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    # Define Output paths
     dpath = broot.parent / "derivatives" / "pylossless" / f"sub-{subject}"
     dpath.mkdir(exist_ok=True, parents=False)
     dpath = dpath / f"ses-{session}"
     dpath.mkdir(exist_ok=True, parents=False)
     dname = f"sub-{subject}_ses-{session}_task-{task}_desc-cleaned_eeg.fif"
+
+    # check when the file was last processed
     if (dpath / dname).exists():
-        warn(f"SKIPPING: {dname} output already exists at {dpath}.")
+        mtime = os.path.getmtime(dpath / dname)
+        time_str = time.ctime(mtime)
+        dt_processed = datetime.strptime(time_str, "%a %b %d %H:%M:%S %Y")
+    else:
+        dt_processed = None
+    if (dpath / dname).exists() and not overwrite:
+        logger.info(f"SKIPPING: {dname} output already exists at {dpath}.")
+        close_logger(logger, console_handler, file_handler)
         return
+    started_processing_date = datetime(2024, 11, 4)
+    if dt_processed and dt_processed >= started_processing_date:
+        logger.info(
+            f"sub-{subject}_ses-{session}_task-{task} processed on {dt_processed}. Skipping"
+            )
+        close_logger(logger, console_handler, file_handler)
+        return
+
+    # If script makes it to this point. We are processing
+    logger.info(f"Processing sub-{subject}_ses-{session}_task-{task}")
+    config_fpath = get_pylossless_config(bpath)
+
+    raw = mne_bids.read_raw_bids(bpath)
+    raw.info["bads"].extend(["E125", "E126", "E127", "E128"])
 
     # Find Breaks
     break_annots = mne.preprocessing.annotate_break(raw)
@@ -41,18 +85,30 @@ def process_one_subject(bpath):
     # PyLossless Pipeline
     pipeline = ll.LosslessPipeline(config_fpath)
     pipeline.run_with_raw(raw.load_data())
-    rejection_policy = ll.RejectionPolicy()
+    rejection_policy = ll.RejectionPolicy(ch_flags_to_reject=['volt_std', 'noisy', 'uncorrelated', 'bridged'])
     cleaned_raw = rejection_policy.apply(pipeline)
 
     basename = f"sub-{subject}_ses-{session}_task-{task}"
     eeg_out = dpath / f"{basename}_desc-cleaned_eeg.fif"
-    cleaned_raw.save(eeg_out)
+    cleaned_raw.save(eeg_out, overwrite=overwrite)
     ica1_out = dpath / f"{basename}_desc-fastica_ica.fif"
     ica2_out = dpath / f"{basename}_desc-infomax_ica.fif"
-    pipeline.ica1.save(ica1_out)
-    pipeline.ica2.save(ica2_out)
+    pipeline.ica1.save(ica1_out, overwrite=overwrite)
+    pipeline.ica2.save(ica2_out, overwrite=overwrite)
     labels_out = dpath / f"{basename}_iclabels.csv"
     pipeline.flags["ic"].to_csv(labels_out)
+    logger.info(f"Finished sub-{subject}_ses-{session}_task-{task}")
+    logger.removeHandler(file_handler)
+    logger.removeHandler(console_handler)
+    file_handler.close()
+    console_handler.close()
+
+
+def close_logger(logger, console_handler, file_handler):
+    logger.removeHandler(file_handler)
+    logger.removeHandler(console_handler)
+    file_handler.close()
+    console_handler.close()
 
 
 def get_pylossless_config(bpath):
@@ -62,6 +118,13 @@ def get_pylossless_config(bpath):
     config_fpath = broot.parent / "listen_pylossless_config.yaml"
     if not config_fpath.exists():
         config.load_default()
+        config["flag_channels_fixed_threshold"] = {}
+        config["flag_channels_fixed_threshold"]["threshold"] = 0.00015
+
+        config["flag_epochs_fixed_threshold"] = {}
+        config["flag_epochs_fixed_threshold"]["threshold"] = 0.00015
+
+
         config["filtering"]["notch_filter_args"]["freqs"] = [60]
         config["noisy_channels"]["outliers_kwargs"]["k"] = 6
         config["noisy_channels"]["outliers_kwargs"]["lower"] = 0.25
@@ -87,7 +150,7 @@ def get_pylossless_config(bpath):
     return config_fpath
 
 
-def process_dataset(broot, task=None):
+def process_dataset(broot, task=None, overwrite=False):
     csv_fpath = broot.parent / "eeg_list.csv"
     df = pd.read_csv(csv_fpath, header=0)
 
@@ -98,7 +161,7 @@ def process_dataset(broot, task=None):
             warn(f"SKIPPING: sub-{subject}_ses-{session}_task-{tup.task} because only the {task} task was requested")
             continue
         bpath = mne_bids.BIDSPath(subject=subject, session=session, task=tup.task, root=broot)
-        process_one_subject(bpath)
+        process_one_subject(bpath, overwrite=overwrite)
 
 
 if __name__ == "__main__":
@@ -113,20 +176,27 @@ if __name__ == "__main__":
         choices=["semantics", "phonemes", "resting"]
         )
     parser.add_argument("--bids_root", dest="bids_root", type=Path, default=None)
+    parser.add_argument(
+        "--overwrite",
+          dest="overwrite",
+          type=bool,
+          choices=[True, False],
+          default=False
+          )
     args = parser.parse_args()
 
     # MNE-BIDS
     broot = args.bids_root
     if broot is None:
-        broot = kinnd.paths.listen_path().parent / "data" / "bids"
+        broot = kinnd.paths.get_listen_path() / "data" / "bids"
     broot = Path(broot).expanduser().resolve()
 
     subject = args.subject
     if subject is None:
-        process_dataset(broot, task=args.task)
+        process_dataset(broot, task=args.task, overwrite=args.overwrite)
     else:
         subject = f"{args.subject}"
         session = args.session.zfill(2)
         task = f"{args.task}"
         bpath = mne_bids.BIDSPath(subject=subject, session=session, task=task, root=broot)
-        process_one_subject(bpath)
+        process_one_subject(bpath, overwrite=args.overwrite)
